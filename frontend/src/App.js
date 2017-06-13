bef260f56393f953f54c0cba1d4d4081ace55b9a
ahmed <ahmed.k.alabdullah@saic.com>
2017-06-13 17:34:36 -0400

    added D3 component from tutorial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import D3 from './components/D3' 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div style={{marginTop:'10px'}}> 
+          <D3 data ={[5,10,1,13]} size ={ [500,500] } ></D3>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
