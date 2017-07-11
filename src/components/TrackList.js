be77b0983780ba473599d4a97936004f745683a0
ed-khalid <ed.khalid1@gmail.com>
2017-07-10 21:16:22 -0400

    clamp and scale

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -4,7 +4,7 @@ export default  class TrackList extends Component {
 
     render() {
 
-        return <div>  
+        return <div style={{float:'left'}}>  
             <h3>Track List</h3>
             <ol> 
                 {this.props.songs.map(it => <li key={it.title}>{it.title}({it.score}) </li>)}
