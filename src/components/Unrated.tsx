6852602b9555107934e5f6648a286f5ee0e96b5e
Ed Khalid <ed.khalid1@gmail.com>
2020-07-02 00:29:21 -0400

    rater drag drop

--- src/components/Unrated.tsx
+++ src/components/Unrated.tsx
@@ -1,13 +1,31 @@
 import { Item } from "../models/Item";
-import React from "react";
+import React, { DragEvent } from "react";
+import './Unrated.css';
 
 
 interface Props {
     items:Item[];
 }
 
-export const Unrated:React.FunctionComponent<Props> = ({items}:{items:Item[]}) => (
-    <ul>
-        {items.map(item => <li key={item.name}>{item.name}</li> )}
-    </ul>
-);
+export const Unrated:React.FunctionComponent<Props> = ({items}:{items:Item[]}) => {
+    const dragStart = (evt:DragEvent<HTMLDivElement>) => {
+        let element = evt.target as HTMLDivElement; 
+            requestAnimationFrame( () => {
+                if (element != null) {
+                  element.classList.add('hide');
+                }
+            })
+    }
+    const dragEnd =(e:DragEvent<HTMLDivElement>) => {
+        let element =  e.target as HTMLDivElement;
+        if (element != null) {
+          element.classList.remove('hide');
+        }
+    }
+    return(
+        <ol>
+            {items.map(item => <li key={item.name}><div draggable="true" onDragStart={dragStart} onDragEnd={dragEnd}>{item.name}</div></li> )}
+        </ol>
+    );
+};
+
