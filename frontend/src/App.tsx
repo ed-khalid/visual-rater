aee2c904fd43e30600f92472b1b970f235040627
Ed Khalid <ed.khalid1@gmail.com>
2020-07-02 00:29:21 -0400

    rater drag drop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Unrated } from './components/Unrated';
 import { initialModel } from './models/initialModel';
+import { UnratedNav } from './components/UnratedNav';
+import { Rater } from './components/Rater';
 
 function App() {
   return (
     <div className="App">
       <header>VisRater</header>
       <div className="main">
+        <div className="presection">
+          { initialModel.length > 8 && 
+          <UnratedNav></UnratedNav> }
+        </div>
         <div className="section left">
           <Unrated items={initialModel}> </Unrated>
         </div> 
         <div className="section right">
+          <Rater></Rater>
         </div>
       </div>
     </div>
