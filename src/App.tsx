4f3deb3d2e12c2ea8788653684e0af90f0e5201b
Ed Khalid <ed.khalid1@gmail.com>
2020-07-05 00:17:34 -0400

    dropping on rater updates without rater

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,63 @@ import { Unrated } from './components/Unrated';
 import { initialModel } from './models/initialModel';
 import { UnratedNav } from './components/UnratedNav';
 import { Rater } from './components/Rater';
+import { Item } from './models/Item';
+import { Position} from './models/Position';
+
+type AppState = {
+  unratedItems:Item[];
+  ratedItems:Item[];
+  rater : {
+    position: Position 
+  }
+  draggedItem?:Item|undefined;
+  draggedItemIsAboveRater:boolean;
+} 
 
 function App() {
+  const [unratedItems, updateUnratedItems] = useState(initialModel);  
+  const [ratedItems, updateRatedItems] = useState<Item[]>([]); 
+  const [draggedItem, updateDraggedItem] = useState<Item|undefined>(undefined); 
+  const [draggedItemIsAboveRater, updateDraggedItemIsAboveRater] = useState(false); 
 
-  const [items, updateItems] = useState(initialModel);  
+  const appState:AppState = {
+    unratedItems,
+    ratedItems,
+    rater: {
+      position : {
+        x: 500,
+        y: 0
+      }
+    },
+    draggedItem,  
+    draggedItemIsAboveRater
+  }
+
   return (
     <div className="App">
       <header>VisRater</header>
       <div className="main">
-        <div className="presection">
-          { items.length > 8 && 
+        <div
+        >
+          { appState.unratedItems.length > 8 && 
           <UnratedNav></UnratedNav> }
         </div>
-        <div className="section left">
-          <Unrated items={items}> </Unrated>
-        </div> 
-        <div className="section right">
-          <Rater items={items} onAddItem={updateItems}></Rater>
-        </div>
+        <svg viewBox="0 0 1000 700">
+          <Unrated 
+                   unratedItems={appState.unratedItems} 
+                   ratedItems={appState.ratedItems} 
+                   onDrag={updateDraggedItem}
+                   onRater={updateDraggedItemIsAboveRater}
+                   updateItems={[updateUnratedItems, updateRatedItems]} 
+          > 
+          </Unrated>
+          <Rater 
+                 highlight={appState.draggedItemIsAboveRater} 
+                 position={appState.rater.position}
+                 ratedItems={appState.ratedItems}  
+          >
+           </Rater>
+        </svg>
       </div>
     </div>
   );
