36e0c003ed1ddae9d58ae571377d18c56c031d5d
Ed Khalid <ed.khalid1@gmail.com>
2020-07-05 23:54:06 -0400

    add models

---
+++ frontend/src/models/RatedItem.ts
@@ -0,0 +1,11 @@
+import { Item } from './Item';
+
+
+export class RatedItem {
+
+    public constructor(public item:Item, public score:number) {
+
+    }
+
+
+} 
---
+++ frontend/src/models/music/Album.ts
@@ -0,0 +1,10 @@
+import { Artist } from "./Artist";
+
+
+export class Album {
+
+    constructor(public id:number, public name:string, public artist:Artist) {
+
+    }
+
+}   
