7a59ef8888200856a38d7a14f18b1752bf4540e2
Ed Khalid <ed.khalid1@gmail.com>
2021-01-27 12:16:36 -0500

    dashboard album -> dashboardlabumid

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -6,24 +6,23 @@ import { DashboardAlbumSummary } from "./DashboardAlbumSummary"
 
 interface Props {
     artists:Artist[]
-    selectedAlbum:Album|undefined
-    onAlbumSelect:(album:Album|undefined,artist:Artist|undefined) => void
-    selectedArtist:Artist|undefined
+    selectedAlbum:string|undefined
+    onAlbumSelect:(albumId:string|undefined,artistId:string|undefined) => void
+    selectedArtist:string|undefined
 }
 
 export const Dashboard = ({artists,selectedAlbum, selectedArtist, onAlbumSelect}:Props) => {
 
     const setAlbum =  (album:Album, artist:Artist) => {
-        onAlbumSelect(album, artist)
+        onAlbumSelect(album.id, artist.id)
     } 
     const onAlbumClose = () => {
         onAlbumSelect(undefined, undefined)
     } 
-     
 
     return <div id="dashboard" className="flex-column">
         {artists && artists.map(artist =>  <DashboardArtist key={artist.id} onAlbumSelect={setAlbum} artist={artist} />)}
-        {selectedAlbum && selectedArtist && <DashboardAlbumSummary onClose={onAlbumClose}  artistName={selectedArtist.name} album={selectedAlbum} />}
+        {selectedAlbum && selectedArtist && <DashboardAlbumSummary onClose={onAlbumClose} artistName={artists.find(it => it.id === selectedArtist)!.name} album={artists.find(it => it.id === selectedArtist)?.albums?.find(it => it?.id === selectedAlbum )!} />}
     </div>
 
 }
