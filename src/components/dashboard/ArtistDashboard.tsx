03bfaad2b812fec34d349bff3f03584c719262f8
Ed Khalid <ed.khalid1@gmail.com>
2021-01-28 17:27:00 -0500

    album nav

--- src/components/dashboard/ArtistDashboard.tsx
+++ src/components/dashboard/ArtistDashboard.tsx
@@ -1,8 +1,8 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import './ArtistDashboard.css'
 import { Album, Artist, Maybe } from "../../generated/graphql"
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons' 
 interface DashboardArtistProps {
     artist:Artist
     onAlbumSelect:(album:Album, artist:Artist) => void 
@@ -10,13 +10,12 @@ interface DashboardArtistProps {
 
 export const  DashboardArtist = ({artist,  onAlbumSelect}: DashboardArtistProps) => {
     const ALBUMS_PER_ARTIST = 5 
-    const pageNumber = 1
+    const [pageNumber, setPageNumber] = useState<number>(1) 
     const thumbnail = artist.thumbnail || ''  
 
     const sortAlbums =(albums?:Maybe<Album>[]|null) : Maybe<Album>[] => {
         if (albums) {
-            const sortedAlbums= [...albums].sort(byYear)
-            return sortedAlbums
+            return [...albums].sort(byYear)
         }
         return []
     }
@@ -39,11 +38,19 @@ export const  DashboardArtist = ({artist,  onAlbumSelect}: DashboardArtistProps)
         </div>
                 <div className="artist-title font-title" key={"artist" + artist.id}>{artist.name}</div>
                 <div className="dashboard-albums flex">
-                        {sortAlbums(artist.albums).slice(ALBUMS_PER_ARTIST *(pageNumber-1), Math.min(pageNumber *(ALBUMS_PER_ARTIST), artist.albums!.length) ).map(album => 
-                        (album) ? <div key={album.id} onClick={() => onAlbumSelect(album, artist) } className="dashboard-album">
-                            <img alt={album?.name} src={album?.thumbnail || ''} />
-                        </div> : null 
-                        )}
+                        <div className={`dashboard-albums-navigation flex-column ${(pageNumber === 1) ? 'disabled': '' }`} >
+                            <FontAwesomeIcon onClick={() => setPageNumber(pageNumber-1)} icon={faArrowLeft}></FontAwesomeIcon>
+                        </div>
+                        <div className="dashboard-albums-content flex">
+                            {sortAlbums(artist.albums).slice(ALBUMS_PER_ARTIST *(pageNumber-1), Math.min(pageNumber *(ALBUMS_PER_ARTIST), artist.albums!.length) ).map(album => 
+                            (album) ? <div key={album.id} onClick={() => onAlbumSelect(album, artist) } className="dashboard-album">
+                                <img alt={album?.name} src={album?.thumbnail || ''} />
+                            </div> : <div>nothing</div> 
+                            )}
+                        </div>
+                        <div className={`dashboard-albums-navigation flex-column ${pageNumber*ALBUMS_PER_ARTIST >= artist?.albums!.length? "disabled":"" }`}>
+                            <FontAwesomeIcon onClick={() => setPageNumber(pageNumber+1)} icon={faArrowRight}></FontAwesomeIcon>
+                        </div>
                 </div>
     </div>
 
