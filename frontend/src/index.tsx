f16c4c63d3a7f66cf791b4590bd6f731e46f923c
Ed Khalid <ed.khalid1@gmail.com>
2023-07-06 22:13:37 -0400

    upgrade to react 18

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,17 +8,20 @@ import { client } from './setupApollo';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
+const container = document.getElementById('root')
+if (container) {
+  const root = createRoot(container) 
+  root.render(
+    <React.StrictMode>
+      <ApolloProvider client={client}>
+      <DndProvider backend={HTML5Backend}>
+        <App />
+        </DndProvider>
+      </ApolloProvider>
+    </React.StrictMode>
+  )
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-     <DndProvider backend={HTML5Backend}>
-      <App />
-      </DndProvider>
-    </ApolloProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
