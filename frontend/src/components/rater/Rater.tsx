18b3f9dd499d69db51ba2f04c3a1c0be810c774c
Ed Khalid <ed.khalid1@gmail.com>
2023-07-08 00:26:57 -0400

    deleting trees

--- frontend/src/components/rater/Rater.tsx
+++ frontend/src/components/rater/Rater.tsx
@@ -56,82 +56,12 @@ export const Rater = ({position, state, setState, items, setItems, mode}:Props) 
 
     const [updateSong]  = useUpdateSongMutation();
     const [currentItem, setCurrentItem] = useState<RatedItem|null>();  
-    const [singleElements, setSingleElements] = useState<RatedSongItem[]>([])
-    const [trees, setTrees] = useState<RaterTreeInfo[]>([])
+    const [groupedItems, setGroupedItems] = useState<RatedSongItemGrouped[]>([])
     const g = useRef<SVGGElement>(null)
     // const zoomTarget= useRef<SVGGElement>(null)
     // const zoomListener = useRef<SVGRectElement>(null)
     // const [zoomBehavior, setZoomBehavior] = useState<any>() 
 
-
-
-    const drawTrees =  useCallback((groupedItems:RatedSongItemGrouped[])  => {
-        const trees:Array<RaterTreeInfo> = []
-        if (groupedItems.length === 0) {
-            return trees
-        }
-        // sort by position
-        groupedItems.sort((a,b) => (a.position < b.position) ? -1 :(a.position > b.position) ? 1 : 0 ) 
-        // sort by middle then one then down
-        const index = Math.floor(groupedItems.length / 2)
-        const graphSort:RatedSongItemGrouped[] = [groupedItems[index]]
-        for (let i = 1 ; i <= groupedItems.length/2; i++) {
-            if ((index+i) < groupedItems.length) {
-              graphSort.push(groupedItems[index+i]) 
-            }
-            if ((index-i) > -1) {
-              graphSort.push(groupedItems[index-i]) 
-            }
-        }
-
-        const findNewCenter = (mainlineY:number, items:Array<{coord:Position, item:RatedSongItem}>, intersection: RaterTreeInfo) => {
-            const intersectionHalfHeight = (intersection.bottom - intersection.top)/2      
-            const newTreeTop = (mainlineY - items[0].coord.y)  
-            const newTreeBottom = (mainlineY - items[items.length-1].coord.y)  
-            const newTreeHalfHeight  = (newTreeBottom - newTreeTop)/2 
-            const margin = 30
-            let retv;  
-            if (mainlineY >= intersection.center.y) {
-                retv =  intersection.center.y+ intersectionHalfHeight + newTreeHalfHeight + margin
-            } else {
-                retv = intersection.center.y - intersectionHalfHeight - newTreeHalfHeight - margin
-            }
-            return retv
-        } 
-
-        const detectIntersection = (mainlineY:number, tree:RaterTreeInfo, items:Array<{coord:Position, item:RatedSongItem}>) => {
-            const newTreeTop = (mainlineY - items[0].coord.y)  
-            const newTreeBottom = (mainlineY - items[items.length-1].coord.y)  
-            if ( tree.top <= newTreeTop && tree.bottom >= newTreeTop) return true 
-            if ( tree.top <= newTreeBottom && tree.bottom >= newTreeBottom) return true
-            return false
-        }
-
-        graphSort.forEach(group => {
-           const orientation = group.items[0].orientation
-           const anchor = (orientation === RaterOrientation.LEFT) ? (90 * (Math.PI/180)) : (-90 * (Math.PI/180)); 
-           const circleDivisions = group.items.length + 1;   
-           const step = Math.PI / circleDivisions   
-           const radius = 60 + (group.items.length * 7) ; 
-           const items = group.items.map((item, i) => {
-                const cos = radius * Math.cos(anchor + ((i+1)*step) ) 
-                const sin = radius * Math.sin(anchor + ((i+1)*step) ) 
-                return { item, coord: { x: cos, y : sin} }
-           })
-           const intersection  = trees.find(it =>  detectIntersection(group.position, it, items))
-           const center = intersection ? { x: (orientation === RaterOrientation.LEFT) ?position.x-100: position.x+100, y: findNewCenter(group.position, items, intersection)}  : { x: (orientation === RaterOrientation.LEFT)? position.x-100: position.x+100, y: group.position}  
-            trees.push({
-                top: center.y - items[0].coord.y ,
-                bottom: center.y - items[items.length-1].coord.y,
-                items,
-                mainline: {x: position.x, y: group.position },
-                center,
-                stepSize: step,
-                radius: radius 
-            })
-        })
-        return trees
-    }, [position.x])
 
     useEffect(() => {
         const groupCloseItems = (ratedItems:RatedSongItem[]) => {
@@ -145,18 +75,14 @@ export const Rater = ({position, state, setState, items, setItems, mode}:Props) 
                 }
                 return acc
             },  [])
-
-            const singleElements = groupedItems.filter(it => it.items.length === 1).map(it => it.items[0])    
-            const trees = drawTrees(groupedItems.filter(it => it.items.length > 1))
-            return { singleElements, trees } 
+            return groupedItems
         }  
         const leftItems = items.filter( it => it.orientation === RaterOrientation.LEFT)
         const rightItems = items.filter( it => it.orientation === RaterOrientation.RIGHT)
         const leftGroups =  groupCloseItems(leftItems)
         const rightGroups =  groupCloseItems(rightItems)
-        setSingleElements([...leftGroups.singleElements, ...rightGroups.singleElements])
-        setTrees([...leftGroups.trees, ...rightGroups.trees])
-    }, [items, state.scaler, drawTrees])
+        setGroupedItems([...leftGroups, ...rightGroups])
+    }, [items, state.scaler])
 
     useEffect(() => {
         switch(state.itemType) {
@@ -196,19 +122,21 @@ export const Rater = ({position, state, setState, items, setItems, mode}:Props) 
                       <g className="zoom-target">
                         <g className={mode === RaterMode.PRIMARY ? "rater-axis" : "rater-axis readonly" }></g>
                         <line className={mode === RaterMode.PRIMARY? "rater-line": 'rater-line readonly' } x1={position.x} y1={0} x2={position.x} y2={position.y} />
-                      { singleElements.map(item => 
+                      { groupedItems.map(group => { 
+                       (group.items.length === 1) ? return 
                             <SingleRaterItem
-                                key={item.id}
-                                item={item}
+                                key={group.items[0].id}
+                                item={group.items[0]}
                                 raterBottom={RATER_BOTTOM}
-                                orientation={item.orientation}
+                                orientation={group.items[0].orientation}
                                 x={position.x}
-                                y={state.scaler.toPosition(item.score)}
+                                y={state.scaler.toPosition(group.items[0].score)}
                                 scaler={state.scaler}
                                 onDragEnd={updateItem}
-                            />)
-                      }
-                      {trees.map(tree => <MultiRaterItem
+                                :
+                            />})}
+                        (group.items.length > 1) && 
+                        <MultiRaterItem
                             key={"tree-" + tree.top}
                             tree={tree}
                             orientation={tree.items[0].item.orientation}
@@ -216,6 +144,7 @@ export const Rater = ({position, state, setState, items, setItems, mode}:Props) 
                             onDragEnd={updateItem}
                       />)
                       }
+                      
                       </g>
                     </g>
             )
