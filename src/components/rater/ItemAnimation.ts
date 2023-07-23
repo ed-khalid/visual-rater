9dc52246a3662765daaec8565f6eff9b95d05031
Ed Khalid <ed.khalid1@gmail.com>
2023-07-23 14:56:17 -0400

    responsive svg with slice try that for now

--- src/components/rater/ItemAnimation.ts
+++ src/components/rater/ItemAnimation.ts
@@ -1,11 +1,11 @@
 import { select } from "d3-selection";
 import { RatedMusicItemUI } from "../../models/ui/ItemTypes";
-import { RATER_TIER_WIDTH, RaterOrientation } from "../../models/ui/RaterTypes";
+import { RATER_TIER_WIDTH, RaterOrientation, SVG_IMAGE_SIZE } from "../../models/ui/RaterTypes";
 import { ANIMATION_DURATION } from "../../models/ui/Animation";
 
     export const animateOnEnter = (item:RatedMusicItemUI, mainlineX:number) => {
         const tierOffset = RATER_TIER_WIDTH * item.tier 
-        const imageSize = 25;
+        const imageSize = SVG_IMAGE_SIZE;
         const x = (item.orientation === RaterOrientation.LEFT) ?(mainlineX - tierOffset) : (mainlineX + tierOffset) 
         select(item.nodeRef.current).select('.item-scoreline').transition().duration(ANIMATION_DURATION).attr('x1', x)
         select(item.nodeRef.current).select('.item-thumbnail').transition().duration(ANIMATION_DURATION).attr('x', x)
@@ -18,7 +18,7 @@ import { ANIMATION_DURATION } from "../../models/ui/Animation";
     export const animateOnExit = (item:RatedMusicItemUI, mainlineX:number)  => {
         const EXIT_DURATION = ANIMATION_DURATION + 200; 
         const tierOffset = RATER_TIER_WIDTH * item.tier 
-        const imageSize = 25;
+        const imageSize = SVG_IMAGE_SIZE;
         const x = (item.orientation === RaterOrientation.LEFT) ?(mainlineX + tierOffset) : (mainlineX - tierOffset) 
         select(item.nodeRef.current).select('.item-thumbnail').transition().duration(EXIT_DURATION).attr('x', x)
         select(item.nodeRef.current).select('.item-thumbnail-overlay').transition().duration(EXIT_DURATION).attr('cx', x+imageSize/2)
